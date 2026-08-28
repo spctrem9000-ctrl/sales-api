f9e1cc51d14e28e7fd56e33985735eee37873c59
spctrem9000-ctrl <spctrem9000@gmail.com>
2026-08-28 17:21:08 +0300

    Fix missing GZipMiddleware import

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 
